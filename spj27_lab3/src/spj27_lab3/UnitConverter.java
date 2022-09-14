d51e6f781e88ce1397b28549add8fdabbfff4785
sseanjacobss <spj27@pitt.edu>
2022-09-14 17:09:22 -0400

    updated version of lab3

--- spj27_lab3/src/spj27_lab3/UnitConverter.java
+++ spj27_lab3/src/spj27_lab3/UnitConverter.java
@@ -6,24 +6,30 @@ public class UnitConverter {
 
 	public static void main(String[] args) {
 		
-		String number = JOptionPane.showInputDialog("Enter a value to convert:");
-		String unit = JOptionPane.showInputDialog("Enter a unit:");
+		// receiving user input and converting the number and unit into two separate variables
+		String input = JOptionPane.showInputDialog("Enter a distance or weight and its associated unit. (Makes sure to include a space in between the number and the unit");
+		String number[] = input.split(" ");
 		
-		double numberDouble = Double.parseDouble(number);
-		String initial = (number + unit);
+		String numbers = number[0];
+		double chosenNumber = Double.parseDouble(numbers);
 		
-		double cmToIn = numberDouble / 2.54;
-		double inToCm = numberDouble * 2.54;
-		double ydToM = numberDouble / 1.094;
-		double mToYd = numberDouble * 1.094;
-		double ozToGm = numberDouble * 28.35;
-		double gmToOz = numberDouble / 28.35;
-		double lbToKg = numberDouble / 2.205;
-		double kgToLb = numberDouble * 2.205;
+		String unit = number[1];
 		
+		// conversion factors being assigned as a double
+		double cmToIn = chosenNumber / 2.54;
+		double inToCm = chosenNumber * 2.54;
+		double ydToM = chosenNumber / 1.094;
+		double mToYd = chosenNumber  * 1.094;
+		double ozToGm = chosenNumber  * 28.35;
+		double gmToOz = chosenNumber  / 28.35;
+		double lbToKg = chosenNumber  / 2.205;
+		double kgToLb = chosenNumber  * 2.205;
+		
+		// creating a string that will be used in all forms of feedback response
+		String initial = (chosenNumber + unit);
 		String display = initial + " = ";
 	
-		
+		// if statements to determine units and conversion factors.
 		if (unit.equalsIgnoreCase("cm")) {
 			System.out.println(display + cmToIn + "in");
 		} else if (unit.equalsIgnoreCase("in")) {
@@ -47,7 +53,6 @@ public class UnitConverter {
 		} else if (unit.equalsIgnoreCase("kg")) {
 			System.out.println(display + kgToLb + "lb");
 		}
-
 	}
 
 }
